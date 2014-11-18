8e46624e9fb06ca48af4f80a8cb941c5a51dfcc0
Rens <Rens@Rens-PC.fritz.box>
2014-11-18 22:32:50 +0100

    k moet nog committen he

--- src/testpackage/NogEenTestClass.java
+++ src/testpackage/NogEenTestClass.java
@@ -4,7 +4,7 @@ public class NogEenTestClass {
 
 	public static void main(String[] args){
 	
-	
+	//mag ik na committen pushen
 	System.out.println("Bullshit dit");
 	//dan mag ik nu nog wat aanpassen?
 	}
