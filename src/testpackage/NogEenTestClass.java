7de8fecf83615f34e46348e0c4be5683b6f03b71
Rens <Rens@Rens-PC.fritz.box>
2014-11-18 22:47:57 +0100

    last for 2day

--- src/testpackage/NogEenTestClass.java
+++ src/testpackage/NogEenTestClass.java
@@ -5,7 +5,7 @@ public class NogEenTestClass {
 	public static void main(String[] args){
 	
 	//mag ik na committen pushen
-	System.out.println("klootzak");
+	System.out.println("heey");
 	//dan mag ik nu nog wat aanpassen?
 	}
 }
