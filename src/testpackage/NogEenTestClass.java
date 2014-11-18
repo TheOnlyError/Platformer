04588d2f9f4433ff830220a20bd55cef4c35ba5c
rerennnn <rehij@hotmail.com>
2014-11-18 22:23:27 +0100

    hmm

--- src/testpackage/NogEenTestClass.java
+++ src/testpackage/NogEenTestClass.java
@@ -3,5 +3,10 @@ package testpackage;
 public class NogEenTestClass {
 //update
 //mag t nu? :D
+	public static void main(String[] args){
+		
+	
 	System.out.println("Bullshit dit");
+	//dan mag ik nu nog wat aanpassen?
+	}
 }
